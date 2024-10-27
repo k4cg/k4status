7b21e6712c65c229270f9ada0d9b5f9d2f191045
Poschi <development@poschi.net>
2024-10-27 21:29:44 +0100

    Added template.json to package

--- default.nix
+++ default.nix
@@ -4,4 +4,8 @@ in pkgs.rustPlatform.buildRustPackage rec {
   version = "0.1";
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
+  postInstall = ''
+    mkdir -p $out/conf
+    cp template.json $out/conf/template.json
+  '';
 }
