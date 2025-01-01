4bf3fbc6ff3c3f863382be095b1369676195f554
Max <max@karl.wf>
2025-01-01 16:48:32 +0100

    Fix dependency version in default.nix

--- default.nix
+++ default.nix
@@ -2,7 +2,12 @@ let pkgs = import <nixpkgs> { };
 in pkgs.rustPlatform.buildRustPackage rec {
   pname = "k4status";
   version = "1.1.0";
-  cargoLock.lockFile = ./Cargo.lock;
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "spaceapi-0.9.0" = "sha256-pfo/JiFetFc/vS2AYQBAukdjxqmdp0Nh/ebwj4C2a10=";
+    };
+  };
   src = pkgs.lib.cleanSource ./.;
   postInstall = ''
     mkdir -p $out/conf
